41ceb676c2a33d43b9a12fdd7e26b8e37e1a3d26
Katia Jauregui <katiajauregui@gmail.com>
2023-02-06 20:53:02 -0600

    add swagger

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const app = require('./app');
 require('dotenv').config();
+const swaggerDocs = require('../swagger');
 
 const PORT = process.env.PORT || 8000;
 
-const swaggerDocs = require('../swagger');
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
